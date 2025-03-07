b7fc792d88233450ab77a7f5a3c615c5492dae7d
aslanovalisher2213@gmail.com <aslanovalisher2213@gmail.com>
2025-03-08 01:15:21 +0500

    cw_2

---
+++ cw_2/post/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import post_list, post_detail, post_create, post_delete
+
+urlpatterns = [
+    path('posts/', post_list, name='post_list'),
+    path('posts/<int:post_id>/', post_detail, name='post_detail'),
+    path('posts/create/', post_create, name='post_create'),
+    path('posts/<int:post_id>/delete/', post_delete, name='post_delete'),
+]
